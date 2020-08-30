6043dc6ef097a9fd09ba12609123e4304f862c62
OsmanAliJunior <odiyar@mail.ru>
2020-08-30 19:18:47 +0300

    test

--- wheel_of_fate_support/controllers/Shifts.js
+++ wheel_of_fate_support/controllers/Shifts.js
@@ -1,32 +1,32 @@
 const Shifts = require('../models/Shifts');
 const AppError = require('../utils/appError');
 
-exports.createShift = async (req, res, next) => {
+exports.createShift = async (req, res, next) =>
+{
     const result = await Shifts.create(req.body);
 
-    if(!result){
-        return next(new AppError("Could not create! Something went wrong!"))
+    if (!result)
+    {
+        return next(new AppError('Could not create! Something went wrong!'));
     }
 
     res.status(201).json({
         status: 'Success',
-        data: result,
-    })
-}
+        data: result
+    });
+};
 
-exports.getShifts = async (req, res, next) => {
+exports.getShifts = async (req, res, next) =>
+{
     const doc = await Shifts.find({}, {__v: 0});
 
-    if(!doc){
-        res.send("Somethng went wrong")
+    if (!doc)
+    {
+        res.send('Somethng went wrong');
     }
     res.send({
-     result: doc
-    })
-}
-
-exports.updateShifts = async (req, res, next) => {
-
-}
+        result: doc
+    });
+};
 
 
